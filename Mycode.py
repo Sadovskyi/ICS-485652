a6563aedd644ddadb3cf2d9ec4ce662a04f18dea
Sadovskyi <90486851+Sadovskyi@users.noreply.github.com>
2022-08-01 23:30:42 +0300

    Delete Mycode.py

--- Mycode.py
+++
@@ -1,9 +0,0 @@
-name = input ("Ваше ім'я: ")
-surname = input ("Ваша прізвище: ")
-rikNarodzhenya = int (input("Ваш рік народження: "))
-potochniyRik = int (input("Поточний рік: "))
-age = potochniyRik - rikNarodzhenya 
-if age >= 18:
-    print (name + " " + surname + " є повнолітнітнім(ньою)")
-else: 
-    print (name + " " + surname + " є неповнолітнім(ньою)")
